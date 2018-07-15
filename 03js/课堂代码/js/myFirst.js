c251543b4abf8ab1b3faf3dc9f1a07afc35d0344
cleaneye213 <cleaneye213@126.com>
2018-07-15 17:36:58 +0800

    增加课堂代码

---
+++ 03js/课堂代码/js/myFirst.js
@@ -0,0 +1,12 @@
+
+//<script type="text/javascript">
+var myjs="ssss";//全局变量
+alert("在js里"+myjs);
+
+var str="1111";
+function firstFun(){
+	var funVar="ddd";
+	alert(myjs);
+}
+//</script>
+/*這是注释*/
